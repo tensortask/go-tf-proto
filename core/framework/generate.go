8e01035a76a37ca3f7b4f8410edc755f5401c959
sbsends <sbsends@gmail.com>
2018-12-21 00:24:29 -0600

    core/framework: seperated packages

--- core/framework/generate.go
+++ core/framework/generate.go
@@ -30,7 +30,7 @@ func generateProtos() error {
 		fmt.Sprintf("-I=%s", filepath.Join(goPath, "src")),
 		fmt.Sprintf("-I=%s", filepath.Join(goPath, "src", "github.com", "gogo", "protobuf", "protobuf")),
 		fmt.Sprintf("--proto_path=%s", filepath.Join(goPath, "src", "github.com")),
-		"--gogofaster_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:.",
+		"--gogofaster_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:gen",
 	}
 	args = append(args, files...)
 
